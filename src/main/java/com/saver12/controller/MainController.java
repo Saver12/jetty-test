16800237470a1b7dd75254d872f508a9dc36d62e
Sergei Ermushin <sergei.ermushin@kronos.com>
2019-11-04 16:43:18 +0300

    rename method

--- src/main/java/com/saver12/controller/MainController.java
+++ src/main/java/com/saver12/controller/MainController.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 public class MainController {
 
    @GET
-   public String getAllUsers() {
+   public String getMessage() {
       return "Hello World!!!";
    }
 }
