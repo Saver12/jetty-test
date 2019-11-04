b2044e0b63849c2e1ac6520bde4b4194d6bbaee1
Sergei Ermushin <sergei.ermushin@kronos.com>
2019-11-04 16:36:59 +0300

    init

---
+++ src/main/java/com/saver12/config/ApplicationConfig.java
@@ -0,0 +1,12 @@
+package com.saver12.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ApplicationConfig extends ResourceConfig {
+
+   private static final String PACKAGES = "com.saver12.controller";
+
+   public ApplicationConfig() {
+      packages(true, PACKAGES);
+   }
+}
---
+++ src/main/java/com/saver12/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.saver12.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+@Produces(MediaType.APPLICATION_JSON)
+public class MainController {
+
+   @GET
+   public String getAllUsers() {
+      return "Hello World!!!";
+   }
+}
